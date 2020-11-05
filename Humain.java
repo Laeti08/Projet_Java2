6c7dc580f410e497fee2602c03d4d8daead6fc6f
Laetitia <laetitia.husson@student.yncrea.fr>
2020-11-05 10:57:35 +0100

    insertion package

--- Humain.java
+++ Humain.java
@@ -1,6 +1,25 @@
-package western
+package western;
 
 public class Humain{
+    String nom;
+    String boissonFavorite;
 
-    
+    //méthode
+    public void parle (String){
+
+    }
+
+    public void sePresenter(){
+
+    }
+
+    public String quelEstTonNom (){
+
+    }
+
+    public String getBoisson () {
+        
+    }
+
+
 }
