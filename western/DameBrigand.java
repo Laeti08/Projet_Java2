ca64095ca3449cca48294e2b44b96e1e2d041deb
Laetitia <laetitia.husson@student.yncrea.fr>
2020-11-19 11:53:17 +0100

    encore des reglages

--- western/DameBrigand.java
+++ western/DameBrigand.java
@@ -3,7 +3,7 @@ package western;
 public class DameBrigand extends DameDetresse implements HorsLaLoi{
 
     //attributs 
-    public int nbDameEnlevees;
+    public int nbDamesEnlevees;
     public int recompense;
     public String look;
     public Boolean estEnPrison;
@@ -12,7 +12,7 @@ public class DameBrigand extends DameDetresse implements HorsLaLoi{
 
     public DameBrigand(String nom, String boissonFavorite, String etat, String couleurRobe, int nbDameEnlevees, int recompense, String look, Boolean estEnPrison){
         super(nom,boissonFavorite,etat,couleurRobe);
-        this.nbDameEnlevees = nbDameEnlevees;
+        this.nbDamesEnlevees = nbDamesEnlevees;
         this.recompense = recompense;
         this.look = look;
         this.estEnPrison = estEnPrison;
@@ -21,18 +21,28 @@ public class DameBrigand extends DameDetresse implements HorsLaLoi{
     @Override
     public void seFaireEmprisonner(Cowboy nomCowboy) {
         // TODO Auto-generated method stub
+        this.estEnPrison = true ;
+        this.nbDamesEnlevees = 0;
+        this.getRecompense();
+        this.recompense = 0;
+        this.parle("Ne crois pas que " + this.nom + " a dit son dernier mot ! " + nomCowboy.nom +" ! Je te retrouverai, et ma vengeance sera terrible ! ");
 
     }
 
     @Override
     public void kidnapperDame(DameDetresse nomDame) {
         // TODO Auto-generated method stub
+        this.recompense += 500;
+        this.nbDamesEnlevees += 1;
+        System.out.println(this.nom + " a kidnappé " + nomDame.nom + " ! " + " Sa récompense vient de monter à " + this.recompense + " dollars ! Le shérif désespère de voir " + this.nbDamesEnlevees + " sous le joug de ce brigand ! " );
 
     }
 
     @Override
     public String getRecompense() {
         // TODO Auto-generated method stub
-        return null;
+        System.out.println("En remettant " + this.nom + "au shérif, vous avez gagné une récompense de " + Integer.toString(this.recompense) + "dollars. Bien joué ! ");
+        return Integer.toString(this.recompense);
+        
     }
 }
--- western/Ripoux.java
+++ western/Ripoux.java
@@ -20,19 +20,28 @@ public class Ripoux extends Sherif implements HorsLaLoi{
     @Override
     public void seFaireEmprisonner(Cowboy nomCowboy) {
         // TODO Auto-generated method stub
-
+        this.estEnPrison = true ;
+        this.nbDamesEnlevees = 0;
+        this.getRecompense();
+        this.recompense = 0;
+        this.parle("Ne crois pas que " + this.nom + " a dit son dernier mot ! " + nomCowboy.nom +" ! Je te retrouverai, et ma vengeance sera terrible ! ");
     }
 
     @Override
     public void kidnapperDame(DameDetresse nomDame) {
         // TODO Auto-generated method stub
-
+        
+        this.recompense += 500;
+        this.nbDamesEnlevees += 1;
+        System.out.println(this.nom + " a kidnappé " + nomDame.nom + " ! " + " Sa récompense vient de monter à " + this.recompense + " dollars ! Le shérif désespère de voir " + this.nbDamesEnlevees + " sous le joug de ce brigand ! " );
     }
 
     @Override
     public String getRecompense() {
         // TODO Auto-generated method stub
-        return null;
+        System.out.println("En remettant " + this.nom + "au shérif, vous avez gagné une récompense de " + Integer.toString(this.recompense) + "dollars. Bien joué ! ");
+        return Integer.toString(this.recompense);
+        
     }
 
 }
